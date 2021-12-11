a07bd5757003552f564f19ccbc0b591ad62b9989
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-12-11 00:07:41 -0500

    Initial Unstyled Code, Compiling error to be fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ThreadDisplay from './ThreadDisplay/components/ThreadDisplay';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const config =
+    {
+      PutYouOwn: 'firebaseConfigInHere'
+    };
+
+    this.app = firebase.initializeApp(config);
+    this.database = this.app.database();
+  }
+
+  render() {
+    return (
+      <ThreadDisplay database={this.database} />
+    );
+  }
 }
 
 export default App;
